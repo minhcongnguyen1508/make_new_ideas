757800dea09eb5eeb38e62d050cceb36433b4779
Nguyen Sy Viet <36764859+nguyensiviet1999@users.noreply.github.com>
2020-11-20 22:30:08 +0700

    homepage complete

--- app/Http/Controllers/Frontend/HomepageController.php
+++ app/Http/Controllers/Frontend/HomepageController.php
@@ -13,6 +13,20 @@ class HomepageController extends Controller
   public function index(){
     $category = DB::table('categories')->get();
     $notifications = DB::table('notifications')->where('notifiable_id',Auth::id())->get();
-    return view('frontend.homepage', ['category' => $category,'notifications'=> $notifications]);
+    $stories = DB::table('posts')->paginate(3);
+    $user_stories = DB::table('users')->join('posts', 'users.id','=', 'posts.user_id')->paginate(3);
+    $the_most_stories_ids = DB::table('likes')
+                          ->select(DB::raw('count(user_id) as user_count, post_id'))
+                          ->groupBy('post_id')
+                          ->orderBy('user_count', 'desc')
+                          ->get();
+    $the_most_stories = DB::table('users')->join('posts', 'users.id','=', 'posts.user_id')
+                        ->whereIn('posts.id',[$the_most_stories_ids[0]->post_id,
+                                        $the_most_stories_ids[1]->post_id,
+                                        $the_most_stories_ids[2]->post_id,
+                                        $the_most_stories_ids[3]->post_id])
+                        ->get();
+    $newest_stories = DB::table('users')->join('posts', 'users.id','=', 'posts.user_id')->orderByRaw('posts.created_at DESC')->limit(5)->get();
+    return view('frontend.homepage', ['newest_stories'=> $newest_stories,'the_most_stories'=>$the_most_stories,'stories'=>$user_stories,'category' => $category,'notifications'=> $notifications]);
   }
 }
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -6,11 +6,11 @@ HEADER
 		<div class="pl-4 pr-0 h-100 tofront">
 			<div class="row justify-content-between">
 				<div class="col-md-6 pt-6 pb-6 align-self-center">
-					<h1 class="secondfont mb-3 font-weight-bold">Medium is an HTML Bootstrap Template for Professional Blogging</h1>
+					<h1 class="secondfont mb-3 font-weight-bold">{{$newest_stories[0]->title}}</h1>
 					<p class="mb-3">
-						Beautifully crafted with the latest technologies, SASS & Bootstrap 4.1.3, Medium is the perfect design for your professional blog. Homepage, post article and category layouts available.
+						{!!$newest_stories[0]->content!!}
 					</p>
-					<a href="{{route('story', 1)}}" class="btn btn-dark">Read More</a>
+					<a href="{{route('story', $newest_stories[0]->id)}}" class="btn btn-dark">Read More</a>
 
 				</div>
 				<div class="col-md-6 d-none d-md-block pr-0" style="background-size:cover;background-image:url({{asset('assets/img/demo/home.jpg')}});">	</div>
