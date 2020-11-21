30ba6ed270e26ae1aeebfe2f0cddc84d276278ca
Nguyen Thu Trang <trangntt.uet@gmail.com>
2020-11-21 12:26:34 +0700

    update readinglist migration

---
+++ database/migrations/2020_11_21_122511_create_table_reading_lists_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableReadingListTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reading_lists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('post_id')->constrained('posts');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reading_lists');
+    }
+}
