b5e75d834397c49259fba67312852ef6590a672d
Nguyen Sy Viet <36764859+nguyensiviet1999@users.noreply.github.com>
2020-12-12 12:47:14 +0700

    predict-category nhưng bỏ phần giả word

--- resources/views/frontend/create-story.blade.php
+++ resources/views/frontend/create-story.blade.php
@@ -18,7 +18,7 @@ MAIN
                         </ul>
                     </div>
                 @endif
-                <form class="form-group" action="{{url('post-story')}}" method="post" enctype="multipart/form-data">
+                <form class="create_form form-group" action="{{url('post-story')}}" method="post" enctype="multipart/form-data">
                     {{ csrf_field() }}
                     <label for="title">Title</label>
                     <input id="title" class="form-control" type="text" name="title">
@@ -27,12 +27,12 @@ MAIN
                     <label for="title">Category</label>
                     <select class="form-control" name="category" id="category">
                         @foreach ($categories as $category)
-                            <option value="{{$category->id}}">{{$category->name}}</option>
+                            <option data-name="{{$category->name}}" value="{{$category->id}}">{{$category->name}}</option>
                         @endforeach
                     </select>
                     <label for="title">Content</label>
                     <textarea class="form-control" name="content" id="content" cols="30" rows="10"></textarea>
-                    <input class="form-control btn btn-success " type="submit" value="submit">
+                    <a class="form-control btn btn-success " id="get_category">Create</a>
                 </form>
                
             </div>
@@ -40,7 +40,24 @@ MAIN
     </div>
 
 @include('layouts/footer')
-<script src={{ url('ckeditor/ckeditor.js') }}></script>
+<script>
+    $('body').on('click','#get_category',function(){
+        var data_request = {query: $('#content').val()};
+        $.ajax({
+        url: "http://4252beec7086.ngrok.io/suggest_category",
+        type: "POST",
+        data: JSON.stringify(data_request),
+        success: function( response ) {
+            if (response['statusCode']=="200 Success!") {
+                var value_response = response['category'];
+                $('#category').val($(`option[data-name="${value_response}"]`).val());
+                $('form').submit();
+            }
+        }
+        });
+    });
+</script>
+{{-- <script src={{ url('ckeditor/ckeditor.js') }}></script>
     <script>
     CKEDITOR.replace( 'content', {
         filebrowserBrowseUrl: 'ckfinder/ckfinder.html',
@@ -51,5 +68,5 @@ MAIN
         filebrowserFlashUploadUrl: 'ckfinder/core/connector/php/connector.php?command=QuickUpload&type=Flash'
         
     } );
-    </script>
+    </script> --}}
 @stop
